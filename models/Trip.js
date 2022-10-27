4c5959b7c05b79205a22e684bc13927fb37a40a2
jsong73 <jessicajinsong@gmail.com>
2022-10-27 19:26:07 -0400

    added my index.js model

--- models/Trip.js
+++ models/Trip.js
@@ -12,26 +12,50 @@ Trip.init(
         user_id:{
         type: DataTypes.INTEGER,
         allowNull: true,
+        references:{
+            model: "account",
+            key: "user_id",
+        },
         },
         activity_id:{
         type: DataTypes.INTEGER,
         allowNull: true,
+        references:{
+            model: "activity",
+            key:"id"
+        },
         },
         transportation_id:{
         type: DataTypes.INTEGER,
         allowNull: true,
+        references:{
+            model: "transportation",
+            key: "id",
+        },
         },
         flight_id:{
         type: DataTypes.INTEGER,
         allowNull: true,
+        references:{
+            model:"flight",
+            key:"id",
+        },
         },
         hotel_id:{
         type: DataTypes.INTEGER,
         allowNull: true,
+        references:{
+            model: "hotel",
+            key: "id",
+        },
         },
         restaurant_id:{
         type: DataTypes.INTEGER,
         allowNull: true,    
+        references:{
+            model: "restaurant",
+            key:"id",  
+        },
         },
     },
     {
--- models/index.js
+++ models/index.js
@@ -6,4 +6,54 @@ const Restaurant = require("./Restaurant");
 const Transportation = require("./Transportation");
 const Trip = require ("./Trip");
 
+Account.hasMany(Trip, {
+    foreignKey:"user_id",
+    onDelete:"CASCASE",
+});
+
+Trip.belongsTo(Account, {
+    foreignKey:"user_id",
+    onDelete:"CASCASE",
+});
+
+Trip.hasMany(Activity, {
+    foreignKey:"id",
+});
+
+Trip.belongsTo(Transportation, {
+    foreignKey:"id",
+});
+
+Trip.belongsTo(Flight, {
+    foreignKey:"id",
+});
+
+Trip.belongsTo(Restaurant, {
+    foreignKey:"id",
+});
+
+
+Trip.belongsTo(Hotel, {
+    foreignKey:"id",
+});
+
+Activity.belongsTo(Trip, {
+    foreignKey:"activity_id",
+});
+
+Transportation.belongsTo(Trip, {
+    foreignKey:"transportation_id",
+});
+
+Flight.belongsTo(Trip, {
+    foreignKey:"flight_id",
+});
+
+Restaurant.belongsTo(Trip, {
+    foreignKey:"restaurant_id",
+});
+
+Hotel.belongsTo(Trip, {
+    foreignKey:"hotel_id",
+});
 module.exports = { Account , Activity, Flight, Hotel, Restaurant, Transportation, Trip};
