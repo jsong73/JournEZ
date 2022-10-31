8465769ffd8a3540015a37bcd3ee732e407b3caf
jsong73 <jessicajinsong@gmail.com>
2022-10-31 00:57:29 -0400

    fixing my routes and handlebars

--- controllers/pagesRoutes/index.js
+++ controllers/pagesRoutes/index.js
@@ -16,4 +16,12 @@ router.get("/signup", async (req, res) => {
   }
 });
 
+router.get("/signin", async (req, res) => {
+  try{
+    res.render("signin");
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
 module.exports = router;
--- public/assets/index.js
+++ public/assets/index.js
@@ -1,14 +1,32 @@
-const loginFormHandler = async (event) => {
+const signupFormHandler = async (event) => {
     event.preventDefault();
 
-    const email = docu
+    const firstName = document.getElementById("first-name").value.trim();
+    const lastName = document.getElementById("last-name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
 
-fetch("/api/account")
-.then((res) => {
-return res.json();
-})
-.then((data) => {
-console.log(data)    
-})
+if(firstName, lastName, email, username, password) {
+ const response = await fetch("/api/account", {
+    method: "POST",
+    body: JSON.stringify({firstName, lastName, email, username, password}),
+    headers: { "Content-Type": "application/json" },
+ });
 
+if(response.ok){
+    document.location.replace("/")
+} else {
+    alert(response.statusText);
 }
+}
+};
+
+document
+    .getElementById("signupbtn")
+    .addEventListener("click", signupFormHandler);
+
+// document
+//     .getElementById("signinbtn")
+//     .addEventListener("submit", signinFormHandler);
+
