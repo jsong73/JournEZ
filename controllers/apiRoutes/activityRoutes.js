81e710300306036e761185ac0913f8086f1034cf
jsong73 <jessicajinsong@gmail.com>
2022-11-01 00:39:16 -0400

    added my activity api route

--- controllers/apiRoutes/activityRoutes.js
+++ controllers/apiRoutes/activityRoutes.js
@@ -2,7 +2,16 @@ const router = require("express").Router();
 const{ Activity } = require("../../models");
 
 // api/activity
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
+    try{
+        const activityData = await Activity.create(req.body)
+        res.status(200).json({ msg: "Activity was successfully created!", activityData})
+    } catch(err){
+        res.status(400).json(err)
+    }
+});
+
+router.get("/", async (req, res) => {    
     try{
         const activityData = await Activity.findAll({
             include: [
--- controllers/apiRoutes/index.js
+++ controllers/apiRoutes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const tripRoutes = require("./tripRoutes");
 const accountRoutes = require("./accountRoutes");
+const activityRoutes = require("./activityRoutes")
 
 router.use("/account", accountRoutes);
 router.use("/trips", tripRoutes);
