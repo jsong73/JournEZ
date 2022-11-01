7a42e37f3f6af9f5e7476b5ad4a9cae3c5598475
jsong73 <jessicajinsong@gmail.com>
2022-11-01 01:08:24 -0400

    created my hotel api routes

--- controllers/apiRoutes/index.js
+++ controllers/apiRoutes/index.js
@@ -4,12 +4,16 @@ const accountRoutes = require("./accountRoutes");
 const activityRoutes = require("./activityRoutes");
 const flightRoutes = require("./flightRoutes");
 const hotelRoutes = require("./hotelRoutes");
+const restaurantRoutes = require("./restaurantRoutes");
+const transportationRoutes = require("./transportationRoutes")
 
 router.use("/account", accountRoutes);
 router.use("/trips", tripRoutes);
 router.use("/activity", activityRoutes);
 router.use("/flight", flightRoutes);
 router.use("/hotel", hotelRoutes);
+router.use("/restaurant", restaurantRoutes);
+router.use("/transportation", transportationRoutes);
 
 
 module.exports = router;
